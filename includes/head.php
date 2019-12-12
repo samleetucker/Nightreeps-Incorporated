47ef6eedf2fba836b6de307a62af63bbe06568cd
Andy Reyes <andres.reyes@getbraintree.com>
2019-12-12 16:27:50 -0600

    added example of decoded client token

--- includes/head.php
+++ includes/head.php
@@ -1,10 +1,12 @@
 <head>
     <meta charset="UTF-8">
     <title>Nightreeps Inc.</title>
+    <!-- CSS stuff -->
     <link rel=stylesheet id=stylesheet type=text/css href="css/app.css">
-    <link rel=stylesheet type=text/css href="css/overrides.css">
+    <!-- Braintree Stuff -->
     <script src="https://js.braintreegateway.com/web/3.55.0/js/client.min.js"></script>
     <script src="https://js.braintreegateway.com/web/3.55.0/js/hosted-fields.min.js"></script>
+    <!-- Bootstrap stuff -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
--- public_html/basics.php
+++ public_html/basics.php
@@ -10,7 +10,7 @@
   <p align="center">Let's make a simple Hosted Fields page.</p>
   <br></br>
   <p>First we are going to run a script that <a href="https://developers.braintreepayments.com/reference/request/client-token/generate/php">generates le client token!</a></p>
-  <h4><p>Here is the code and its result: </h4>
+  <h4><p>Here is the code and its result: </p></h4>
     <div class="well well-sm"><?php highlight_string("<?php
   \$gateway->clientToken()->generate(
     ['customerId' => 242967293,
@@ -29,6 +29,38 @@
   ));?>"
   document.getElementById("client_token").innerHTML = client_token;
   </script>
+<p>The cool thing about these dudes is that you can actually decode them to see the parameters used in the above code. Lots of useful information can be found in a client token. </p>
+<p><button data-toggle="collapse" data-target="#c2">Here is an example</button>
+  <div id="c2" class="collapse">
+    <pre><code id="decode"></code>
+      "version":2,
+      "authorizationFingerprint":"eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NiIsImtpZCI6IjIwMTgwNDI2MTYtc2FuZGJveCIsImlzcyI6IkF1dGh5In0.eyJleHAiOjE1NzYyMTI1MDEsImp0aSI6IjY4OTU0MGVjLTM1MDQtNGIxNi05YzU3LTZiNmRhMTQ4YjA0NSIsInN1YiI6InR0OHNydHBwOHlmZ2ZnaHAiLCJpc3MiOiJBdXRoeSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6InR0OHNydHBwOHlmZ2ZnaHAiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0Ijp0cnVlfSwicmlnaHRzIjpbIm1hbmFnZV92YXVsdCJdLCJvcHRpb25zIjp7ImN1c3RvbWVyX2lkIjoyNDI5NjcyOTMsIm1lcmNoYW50X2FjY291bnRfaWQiOiJNaW5kU2FwbGluZy1DQUQifX0.LwDd-s35WTMeFScTO9Tlfum0W5pleySG69JYDLJobMJOchWyCHFGe-50G2tMx2x-2eJ3R4qRaWdc2BZNm24Jjg?customer_id=",
+      "configUrl":"https://api.sandbox.braintreegateway.com:443/merchants/tt8srtpp8yfgfghp/client_api/v1/configuration",
+      "graphQL": {
+          "url": "https://payments.sandbox.braintree-api.com/graphql", "date": "2018-05-08"
+      }
+      ,
+      "challenges":[],
+      "environment":"sandbox",
+      "clientApiUrl":"https://api.sandbox.braintreegateway.com:443/merchants/tt8srtpp8yfgfghp/client_api",
+      "assetsUrl":"https://assets.braintreegateway.com",
+      "authUrl":"https://auth.venmo.sandbox.braintreegateway.com",
+      "analytics": {
+          "url": "https://origin-analytics-sand.sandbox.braintree-api.com/tt8srtpp8yfgfghp"
+      }
+      ,
+      "threeDSecureEnabled":true,
+      "paypalEnabled":true,
+      "paypal": {
+          "displayName": "MindSapling", "clientId": null, "privacyUrl": "http://example.com/pp", "userAgreementUrl": "http://example.com/tos", "baseUrl": "https://assets.braintreegateway.com", "assetsUrl": "https://checkout.paypal.com", "directBaseUrl": null, "allowHttp": true, "environmentNoNetwork": true, "environment": "offline", "unvettedMerchant": false, "braintreeClientId": "masterclient3", "billingAgreementsEnabled": true, "merchantAccountId": "MindSapling-CAD", "currencyIsoCode": "CAD"
+      }
+      ,
+      "merchantId":"tt8srtpp8yfgfghp",
+      "venmo":"off",
+      "merchantAccountId":"MindSapling-CAD"
+    </pre>
+  </div>
+</p>
   <p>Next we are going to need to make the Hosted Fields forms. This page is going to use the basic example which is on our
     <a href="https://developers.braintreepayments.com/guides/hosted-fields/setup-and-integration/javascript/v3#basic-integration">Hosted Fields Dev Docs.</a>
   </p>
