e56973555b8d852ca534fb606a723ef57b386f1d
Andy Reyes <andres.reyes@getbraintree.com>
2019-12-11 19:32:23 -0600

    added index

---
+++ public_html/index.php
@@ -0,0 +1,29 @@
+<?php require_once("../includes/braintree_init.php"); ?>
+
+
+<html lang='en'>
+  <head>
+    <?php require_once("../includes/head.php"); ?>
+  </head>
+  <div class="wrapper">
+    <body>
+      <h1> ┬─┬ノ( º _ ºノ) Nightreeps Inc. </h1>
+      <p>
+        Welcome.
+      <p>
+      <p>
+        Nightreeps Inc. is a simple way to sandbox out end to end issues.
+      <p>
+        Currently a work in progress, but most basic features are functioning.
+      <p>
+        Don't forget to open your browser's console for helpful console logging!
+      <p>
+      <!-- <a href='cart.php'><button> Drop-in UI w/ 3D Secure and Kount</button></a>
+      <a href='host3d.php'><button> Basic Hosted Fields</button></a> -->
+      <a href='1pc.php'><button> Legacy | (• ◡•)| (❍ᴥ❍ʋ)</button></a>
+      <a href='basics.php'><button> Nuovo 🍝</button></a>
+
+  </div>
+    </body>
+<?php require_once("../includes/footer.php"); ?>
+</html>
