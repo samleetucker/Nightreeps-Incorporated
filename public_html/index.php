da00d20878aff331e19746780c1b8662996163c3
Andy Reyes <andres.reyes@getbraintree.com>
2019-12-17 00:46:32 -0600

    edited index for better messaging

--- public_html/index.php
+++ public_html/index.php
@@ -16,19 +16,20 @@
   <a href="#"></a>
 </div>
   <div class="main">
-    <body align="center">
+    <body>
       <br>
+      <h4> Night-what-inc . . ?</h4>
       <p>
-        Nightreeps Inc. is a simple way to sandbox out end to end issues.
+        Nightreeps Inc. is a growing collection of integrations designed to showcase specific aspects of our products, enable end to end testing, and simply provide a showcase of what our product can do
       <p>
-        Currently a work in progress, but most basic features are functioning.
+        This project is being managed on my the <a href="https://github.com/nightreeps-inc/Nightreeps-Incorporated" target="_blank">Nightreeps Inc. GitHub </a>and plans are to make Nightreeps Inc. an open repository where Treeps can clone, make pull requests, and contribute to the project's success.
       <p>
-        Don't forget to open your browser's console for helpful console logging!
+        Check out the <a href="https://github.com/nightreeps-inc/Nightreeps-Incorporated" target="_blank"> GitHub repository </a>to see the amount of work done so far, and be sure to watch or star it to follow along!
       <p>
       <!-- <a href='cart.php'><button> Drop-in UI w/ 3D Secure and Kount</button></a>
       <a href='host3d.php'><button> Basic Hosted Fields</button></a> -->
       <!-- <a href='1pc.php'><button> Legacy | (• ◡•)| (❍ᴥ❍ʋ)</button></a> -->
-      <a href='basic-checkout.php'><button class="button"> Begin </button></a>
+      <a href='basic-checkout.php'><button class="button"> Basic Hosted Fields Integraiton w/ Client Token Demo </button></a>
 
   </div>
 </div>
