b1ea6c570419a7bf6efeb00da57800e8b7008915
Andy Reyes <andres.reyes@getbraintree.com>
2020-01-06 15:49:38 -0600

    playing with stylesheets again

--- includes/head.php
+++ includes/head.php
@@ -5,8 +5,8 @@
     <link rel=stylesheet id=stylesheet type=text/css href="css/stylesheet.css">
     <!-- Braintree Stuff -->
     <script src="https://js.braintreegateway.com/web/dropin/1.21.0/js/dropin.min.js"></script>
-    <script src="https://js.braintreegateway.com/web/3.55.0/js/client.min.js"></script>
-    <script src="https://js.braintreegateway.com/web/3.55.0/js/hosted-fields.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.56.0/js/client.min.js"></script>
+    <script src="https://js.braintreegateway.com/web/3.56.0/js/hosted-fields.min.js"></script>
     <!-- Bootstrap stuff -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
